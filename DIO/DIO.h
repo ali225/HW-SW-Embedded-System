603a51fe3a121c7a4496f165396d4c39799708d6
ali225 <aligamal_25@yahoo.com>
2018-10-19 18:06:54 +0200

    LCD 8 bit

--- DIO/DIO.h
+++ DIO/DIO.h
@@ -11,12 +11,10 @@
 void DIO_VidSetPortDirection(u8 PortNumber, u8 Value);
 void DIO_VidSetPortValue(u8 PortNumber, u8 Value);
 
-void DIO_VidSetPinDirection(u8 PortNumber,u8 Pin, u8 Value);
-void DIO_VidSetPinValue(u8 PortNumber,u8 Pin, u8 Value);
+void DIO_VidSetPinDirection(u8 PortNumber, u8 Pin, u8 Value);
+void DIO_VidSetPinValue(u8 PortNumber, u8 Pin, u8 Value);
 
 u8 DIO_u8GetPortValue(u8 Port);
-u8 DIO_u8GetPinValue(u8 Port,u8 Pin);
-
-
+u8 DIO_u8GetPinValue(u8 Port, u8 Pin);
 
 #endif /* DIO_H_ */
