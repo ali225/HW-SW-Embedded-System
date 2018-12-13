2c05aa2f07d74d17f75f5cb163e11dfa485b944e
ali225 <aligamal_25@yahoo.com>
2018-12-13 10:02:56 +0200

    TestCPreprocessopr

---
+++ TestCPreprocessopr/Debug/src/TestCPreprocessopr.d
@@ -0,0 +1,1 @@
+src/TestCPreprocessopr.o: ../src/TestCPreprocessopr.c
---
+++ TestCPreprocessopr/src/TestCPreprocessopr.c
@@ -0,0 +1,45 @@
+/*
+ ============================================================================
+ Name        : TestCPreprocessopr.c
+ Author      : Eng Ali Gamal
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+#include <stdio.h>
+
+#define height 100
+#define number 3.14
+#define letter 'A'
+#define letter_sequence "ABc"
+#define backslash_char '?'
+
+
+int main(int argt, char **argv)
+{
+	printf("value if height : %d \n",height);
+	printf("value of number : %f \n",number);
+	printf("value of letter : %c \n",letter);
+	printf("value of Letter_sequence : %s \n",letter_sequence);
+	printf("value of backslash_char :%c \n",backslash_char);
+
+}
+#include <stdio.h>
+
+#define height 100
+#define number 3.14
+#define letter 'A'
+#define letter_sequence "ABc"
+#define backslash_char '?'
+
+
+int main(int argt, char **argv)
+{
+	printf("value if height : %d \n",height);
+	printf("value of number : %f \n",number);
+	printf("value of letter : %c \n",letter);
+	printf("value of Letter_sequence : %s \n",letter_sequence);
+	printf("value of backslash_char :%c \n",backslash_char);
+
+}
