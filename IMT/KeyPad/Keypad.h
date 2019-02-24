f1d63d7111370899c3bcdf6811618a9876afa7c9
ali225 <aligamal_25@yahoo.com>
2019-02-24 18:11:21 +0200

    KeyPad/Keypad

---
+++ IMT/KeyPad/Keypad.h
@@ -0,0 +1,15 @@
+/*
+ * Keypad.h
+ *
+ *  Created on: Feb 24, 2019
+ *      Author: Eng Ali Gamal
+ */
+
+#ifndef KEYPAD_KEYPAD_H_
+#define KEYPAD_KEYPAD_H_
+
+void Keypad_vidInit(void);
+u8 Keypad_u8GetButtonStatus(u8 Button);
+
+
+#endif /* KEYPAD_KEYPAD_H_ */
---
+++ IMT/KeyPad/Keypad_Cfg.h
@@ -0,0 +1,24 @@
+/*
+ * Keypad_Cfg.h
+ *
+ *  Created on: Feb 24, 2019
+ *      Author: Eng Ali Gamal
+ */
+
+#ifndef KEYPAD_KEYPAD_CFG_H_
+#define KEYPAD_KEYPAD_CFG_H_
+
+//Keypad Information
+#define Keypad_Port 2
+
+#define R0 0
+#define R1 1
+#define R2 2
+#define R3 3
+
+#define C0 4
+#define C1 5
+#define C2 6
+#define C3 7
+
+#endif /* KEYPAD_KEYPAD_CFG_H_ */
